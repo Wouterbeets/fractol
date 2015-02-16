ab112a766fd77d3d8b1f75f4bddc4e84b5a9247e
Wouter BEETS <wbeets@e2r8p7.42.fr>
2015-02-16 17:55:33 +0100

    binfings

---
+++ includes/mlx_bindings.h
@@ -0,0 +1,93 @@
+
+#ifndef MLX_BINDING_H
+# define MLX_BINDING_H
+
+/*
+ * ** Keyboard
+ * */
+
+# define KEYCODE_ECHAP			65307
+# define KEYCODE_SPACE			32
+# define KEYCODE_BACKSPACE		65288
+# define KEYCODE_TAB			65289
+# define KEYCODE_ENTER			65293
+# define KEYCODE_ALT			65406
+# define KEYCODE_CAPSLOCK		65509
+# define KEYCODE_SHIFT_L		65505
+# define KEYCODE_SHIFT_R		65506
+# define KEYCODE_CTRL_L			65507
+# define KEYCODE_CTRL_R			65508
+# define KEYCODE_CMD_L			65511
+# define KEYCODE_CMD_R			65512
+# define KEYCODE_0				48
+# define KEYCODE_1				49
+# define KEYCODE_2				50
+# define KEYCODE_3				51
+# define KEYCODE_4				52
+# define KEYCODE_5				53
+# define KEYCODE_6				54
+# define KEYCODE_7				55
+# define KEYCODE_8				56
+# define KEYCODE_9				57
+# define KEYCODE_MINUS			45
+# define KEYCODE_EQUAL			61
+# define KEYCODE_A				97
+# define KEYCODE_B				98
+# define KEYCODE_C				99
+# define KEYCODE_D				100
+# define KEYCODE_E				101
+# define KEYCODE_F				102
+# define KEYCODE_G				103
+# define KEYCODE_H				104
+# define KEYCODE_I				105
+# define KEYCODE_J				106
+# define KEYCODE_K				107
+# define KEYCODE_L				108
+# define KEYCODE_M				109
+# define KEYCODE_N				110
+# define KEYCODE_O				111
+# define KEYCODE_P				112
+# define KEYCODE_Q				113
+# define KEYCODE_R				114
+# define KEYCODE_S				115
+# define KEYCODE_T				116
+# define KEYCODE_U				117
+# define KEYCODE_V				118
+# define KEYCODE_W				119
+# define KEYCODE_X				120
+# define KEYCODE_Y				121
+# define KEYCODE_Z				122
+# define KEYCODE_LEFT			65361
+# define KEYCODE_UP				65362
+# define KEYCODE_RIGHT			65363
+# define KEYCODE_DOWN			65364
+# define KEYCODE_PAVNUM_0		65456
+# define KEYCODE_PAVNUM_1		65457
+# define KEYCODE_PAVNUM_2		65458
+# define KEYCODE_PAVNUM_3		65459
+# define KEYCODE_PAVNUM_4		65460
+# define KEYCODE_PAVNUM_5		65461
+# define KEYCODE_PAVNUM_6		65462
+# define KEYCODE_PAVNUM_7		65463
+# define KEYCODE_PAVNUM_8		65464
+# define KEYCODE_PAVNUM_9		65465
+# define KEYCODE_PAVNUM_ENTER	65421
+# define KEYCODE_PAVNUM_STAR	65450
+# define KEYCODE_PAVNUM_PLUS	65451
+# define KEYCODE_PAVNUM_MINUS	65453
+# define KEYCODE_PAVNUM_DOT		65454
+# define KEYCODE_PAVNUM_SLASH	65455
+# define KEYCODE_PAVNUM_EQUAL	65469
+
+/*
+ * ** Mouse
+ * */
+
+# define MOUSE_BUTTON_1			1
+# define MOUSE_BUTTON_2			3
+# define MOUSE_WHEEL_UP			4
+# define MOUSE_WHEEL_DOWN		5
+# define MOUSE_WHEEL_RIGHT		6
+# define MOUSE_WHEEL_LEFT		7
+
+#endif
